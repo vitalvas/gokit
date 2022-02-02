ea0b97560764c6da0f2307c7131765af1ed21e3e
Vitaliy Vasilenko <me@vitalvas.com>
2022-02-02 15:59:33 +0200

    fqdn: add GetDomainNameGuesses

--- fqdn/fqdn.go
+++ fqdn/fqdn.go
@@ -10,3 +10,19 @@ func GetDomainFromHostname(name string) *string {
 
 	return &elems[1]
 }
+
+func GetDomainNameGuesses(name string) []string {
+	var names []string
+	var nameSplited string
+
+	names = append(names, name)
+	nameSplited = name
+
+	for strings.Contains(nameSplited, ".") {
+		result := strings.SplitN(nameSplited, ".", 2)
+		nameSplited = result[1]
+		names = append(names, result[1])
+	}
+
+	return names
+}
