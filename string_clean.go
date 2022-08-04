330af848ee12b35dcfbeeeb3e8136a951d28ab20
Vitaliy Vasilenko <me@vitalvas.com>
2022-08-04 18:00:35 +0300

    add StringClean

---
+++ string_clean.go
@@ -0,0 +1,13 @@
+package gokit
+
+import (
+	"strings"
+	"unicode"
+)
+
+// clean up the non-ASCII characters
+func StringClean(input string) string {
+	return strings.TrimFunc(input, func(r rune) bool {
+		return !unicode.IsGraphic(r)
+	})
+}
