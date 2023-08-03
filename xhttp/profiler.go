543d16913bad692a994b15dc295295d0d96ce7ba
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-03 19:14:04 -0400

    feat: add xhttp profiler

---
+++ xhttp/profiler.go
@@ -0,0 +1,13 @@
+package xhttp
+
+import (
+	"net/http"
+	"net/http/pprof"
+)
+
+func AttachProfiler(mux *http.ServeMux) {
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+}
