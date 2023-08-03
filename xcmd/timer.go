dd9906bf1efa025c48a92fdfe61c72fc7c4cd8c2
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-02 23:12:21 -0400

    feat: add PeriodicRunWithSignal

--- xcmd/timer.go
+++ xcmd/timer.go
@@ -2,6 +2,9 @@ package xcmd
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -15,6 +18,41 @@ func PeriodicRun(ctx context.Context, execute func(ctx context.Context) error, p
 			return ctx.Err()
 
 		case <-timer.C:
+			if err := execute(ctx); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func PeriodicRunWithSignal(
+	ctx context.Context,
+	execute func(ctx context.Context) error,
+	period time.Duration,
+	signals ...os.Signal,
+) error {
+	timer := time.NewTicker(period)
+	defer timer.Stop()
+
+	if signals == nil {
+		signals = append(signals, syscall.SIGUSR1)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+
+	signal.Notify(sigChan, signals...)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case <-timer.C:
+			if err := execute(ctx); err != nil {
+				return err
+			}
+
+		case <-sigChan:
 			if err := execute(ctx); err != nil {
 				return err
 			}
