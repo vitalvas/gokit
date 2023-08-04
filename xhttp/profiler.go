a610f5208bb77bbe964a323400e97a8de1d18361
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-04 14:20:09 -0400

    fix: add pprof.Trace

--- xhttp/profiler.go
+++ xhttp/profiler.go
@@ -10,4 +10,5 @@ func AttachProfiler(mux *http.ServeMux) {
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 }
