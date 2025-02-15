c77838f66b52c455c8b0e41179e41dc3ed1077fd
Vitaliy Vasylenko <me@vitalvas.com>
2025-02-15 15:28:46 -0500

    fix: tests for xconvert

--- xconvert/string_test.go
+++ xconvert/string_test.go
@@ -18,11 +18,11 @@ func TestStringToPointer(t *testing.T) {
 
 	for _, test := range tests {
 		result := StringToPointer(test.input)
-		assert.NotNil(t, result)
-		assert.Equal(t, test.expected, *result)
-
 		if result == nil || *result != test.expected {
 			t.Errorf("StringToPointer(%q) = %v; want %v", test.input, result, test.expected)
 		}
+
+		assert.NotNil(t, result)
+		assert.Equal(t, test.expected, *result)
 	}
 }
